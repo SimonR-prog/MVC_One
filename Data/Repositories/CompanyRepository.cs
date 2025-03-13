f7ea1f3d29de8956be77c88434ebb3b66c1154b5
Simon <152021717+SimonR-prog@users.noreply.github.com>
2025-03-13 17:11:01 +0100

    Made repos and interfaces.

---
+++ Data/Repositories/CompanyRepository.cs
@@ -0,0 +1,9 @@
+﻿using Data.Context;
+using Data.Entities;
+using Data.Interfaces;
+
+namespace Data.Repositories;
+
+public class CompanyRepository(DataContext context) : BaseRepository<CompanyEntity>(context), ICompanyRepository
+{
+}
