3cb05f0901bde34fd7b3aef2f3479b8f9ce1184d
Simon <152021717+SimonR-prog@users.noreply.github.com>
2025-03-13 16:22:58 +0100

    Created base repository, interface, and changed the response and added a content interface to be able to use it.

---
+++ Data/Interfaces/IBaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using ResponseModel.Interfaces;
+using System.Linq.Expressions;
+
+namespace Data.Interfaces;
+
+public interface IBaseRepository<TEntity> where TEntity : class
+{
+    Task<IResponse> CreateAsync(TEntity entity);
+    Task<IResponseContent<IEnumerable<TEntity>>> GetAllAsync();
+    Task<IResponseContent<TEntity>> GetAsync(Expression<Func<TEntity, bool>> predicate);
+    Task<IResponse> ExistsAsync(Expression<Func<TEntity, bool>> predicate);
+    Task<IResponse> UpdateAsync(TEntity entity);
+    Task<IResponse> DeleteAsync(TEntity entity);
+}
