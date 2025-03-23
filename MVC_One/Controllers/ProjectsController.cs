33f748d479e94008d984a5f7a8a1a08916a68b92
Simon <152021717+SimonR-prog@users.noreply.github.com>
2025-03-23 14:21:21 +0100

    Added images and the portal components and controller.

---
+++ MVC_One/Controllers/ProjectsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_One.Controllers;
+
+public class ProjectsController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
