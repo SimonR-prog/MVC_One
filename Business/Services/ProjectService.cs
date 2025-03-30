547b0bfa834d2038b11ba4ef7e7cf4017aa3585d
Simon <152021717+SimonR-prog@users.noreply.github.com>
2025-03-30 15:39:46 +0200

    Starting service

---
+++ Business/Services/ProjectService.cs
@@ -0,0 +1,22 @@
+﻿using Business.Models;
+using ResponseModel.Interfaces;
+
+namespace Business.Services;
+
+public class ProjectService
+{
+    public async Task<IResponse> AddProject()
+    {
+        try
+        {
+            
+        }
+        catch (Exception ex)
+        {
+
+        }
+    }
+
+
+
+}
